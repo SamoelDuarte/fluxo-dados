a5502ea2d55ed6e3deb610aed0fec18cd22cf16a
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-10-18 20:35:12 -0300

    Update CronController.php

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -20,7 +20,7 @@ class CronController extends Controller
     public function getDadoHavan(HttpRequest $request)
     {
 
-       $contato = ContatoDados::where('telefone', '5579998667794')->first();
+       $contato = ContatoDados::where('telefone', $request->input('telefone'))->first();
 
       
         // Simular os dados do contrato, substitua isso com uma lógica real, como uma consulta ao banco de dados
