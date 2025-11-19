216791ef4463c80d1a821935bf5d20acaab68034
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-11-18 22:09:13 -0300

    Update Contato.php

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Contato extends Model
 {
@@ -11,8 +13,19 @@ class Contato extends Model
 
     protected $fillable = ['name'];
 
-    public function dados()
+    /**
+     * Contato tem muitos ContatoDados
+     */
+    public function dados(): HasMany
     {
         return $this->hasMany(ContatoDados::class, 'contato_id');
+    }
+
+    /**
+     * Contato tem muitas Campanhas através da tabela campanha_contato
+     */
+    public function campanhas(): BelongsToMany
+    {
+        return $this->belongsToMany(Campanha::class, 'campanha_contato', 'contato_id', 'campanha_id');
     }
 }
