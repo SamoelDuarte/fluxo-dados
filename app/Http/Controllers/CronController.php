6289376891f61f7ae77d439a3c656db272356591
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-11-21 13:38:03 -0300

    Update CronController.php

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -1569,7 +1569,7 @@ class CronController extends Controller
                 $contatos = DB::table('contato_dados')
                     ->whereIn('contato_id', $campanha->contatos->pluck('id'))
                     ->where('send', 0)
-                    ->limit(30)
+                    ->limit(1)
                     ->get();
 
                 if ($contatos->isEmpty()) {
