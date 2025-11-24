9f31ef48fbf52383804edb91c0bf4c39662115e1
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-11-24 16:28:43 -0300

    Update CronController.php

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -84,7 +84,7 @@ class CronController extends Controller
             "renegociaSomenteDocumentosEmAtraso" => false // Deixe como false ou conforme necessário
         ];
 
-        dd($data);
+        // dd($data);
 
         // Cabeçalhos da requisição
         $headers = [
