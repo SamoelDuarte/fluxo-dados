b64af73ed5238e17eea58127d833b741b61f563b
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-11-24 10:41:08 -0300

    Update CronController.php

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -2053,7 +2053,7 @@ class CronController extends Controller
                         'ValorParcela' => $valorParcela,
                     ];
 
-                    // dd('payload', $payload);
+                    dd('payload', $payload);
 
                     // Envia para API
                     $response = $client->post(
